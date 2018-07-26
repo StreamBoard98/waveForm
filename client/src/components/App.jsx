a023adcef2a48a56ef35dbef12dea266d1e88f43
johndlopez <johnlopez@Johns-MBP-2.T-mobile.com>
2018-07-25 18:51:15 -0700

    rendered basic graphic and counter

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,11 +9,30 @@ class App extends React.Component {
       data: [],
     };
   }
-//
+
+  componentDidMount() {
+    this.buildCanvas();
+  }
+
+  buildCanvas() {
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+    console.log(canvas);
+    let randomHeight = () => {
+      return Math.floor(Math.random() * (canvas.height / 2));
+    }
+    for(let i = 0; i < canvas.width; i += 3) {
+      let generatedHeight = randomHeight();
+      ctx.fillRect(i, (canvas.height / 2), 2, generatedHeight + 1);
+      ctx.fillRect(i, (canvas.height / 2), 2, -generatedHeight);
+    }
+  }
+
   render() {
     return (
       <div className={styles.content}>
-        hello
+        <canvas id="canvas" width="660" height="100">Please upgrade your browser to the latest Chrome version</canvas>
+        <div className={styles.counter}>4:00</div>
       </div>
     );
   }
