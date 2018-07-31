5cfe46dfdc3cd2b2a07501bce11e7db1a329ed88
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-07-31 13:59:11 -0700

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './client/src/index.js',
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [ {
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+            modules: true,
+            localIdentName: "[local]___[hash:base64:5]"
+          }
+        }, ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, '/client/dist'),
+    publicPath: '/public/',
+    filename: 'bundle.js',
+  },
+};
