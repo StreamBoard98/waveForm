59a089fc82ead8e8430c24a530621dcc79ee17f7
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-07-31 12:19:12 -0700

    tests added

--- test.js
+++ test.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import Enzyme, { shallow, mount, render, configure } from 'enzyme';
+import { createSerializer } from 'enzyme-to-json';
 
 //adapter config placement
-import Enzyme from 'enzyme';
-import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import App from './client/src/components/App';
+import sinon from 'sinon';
 
-const db = require('./db/db');
+import 'canvas-prebuilt';
 
-describe('GET request', () => {
-  test('should return song with given id', (done) => {
-    function callback(error, data) {
-      expect(data.id).toBe(11);
-      done();
-    }
-    db.getSong(11, callback);
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<App /> component', () => {
+
+  // it('calls componentDidMount', () => {
+  //   sinon.spy(App.prototype, 'componentDidMount');
+  //   const wrapper = mount(<App />);
+
+  //   expect(App.prototype.componentDidMount.calledOnce).to.equal(true);
+  // })
+
+  // it('renders the App', () => {
+  //   const wrapper = shallow(<App />);
+
+  //   expect(wrapper.find('.waveformHero')).to.have.length(1);
+  // })
+
+  // it('adds correctly', () => {
+  //   expect(1 + 1).toEqual(2);
+  // })
+  it('renders App', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
   });
-
+  // it('should render a canvas element', () => {
+  //   const wrapper = mount(<App />);
+  //   expect(wrapper.find(<)).to.have.length(1);
+  // });
 });
