c7419343284645f7a9e33b7a775354c0e8b137e1
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-07-31 12:19:12 -0700

    tests added

---
+++ jest-css-modules-transform-config.js
@@ -0,0 +1,15 @@
+const path = require('path');  
+const additionalResolvePath = path.resolve(__dirname, 'src', 'additional_modules');
+ 
+module.exports = {
+    sassConfig: {
+        includePaths: [additionalResolvePath],
+        precision: 5,
+    },
+    lessConfig: {
+        paths: [additionalResolvePath],
+    },
+    stylusConfig: {
+        paths: [additionalResolvePath],
+    },
+};
